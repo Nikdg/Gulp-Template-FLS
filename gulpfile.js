29cc75ed7b5d40f38892c3c440bc72bf076264b8
Nikolay <53534953+Nikdg@users.noreply.github.com>
2021-05-25 23:02:14 +0500

    added function js

--- gulpfile.js
+++ gulpfile.js
@@ -81,18 +81,27 @@ function css() {
     .pipe(browsersync.stream())
 }
 
+function js() {
+    return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
+
 function watchFiles(params) {
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
+    gulp.watch([path.watch.js], js)
 }
 
 function clean(params) {
     return del(path.clean)
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html))
+let build = gulp.series(clean, gulp.parallel(js, css, html))
 let watch = gulp.parallel(build, watchFiles, browserSync)
 
+exports.js = js
 exports.css = css
 exports.html = html
 exports.build = build
