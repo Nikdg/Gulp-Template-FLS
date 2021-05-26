e26c2cbc8d0ab3b97efa10a19dce5f67fb1eebbe
Nikolay <53534953+Nikdg@users.noreply.github.com>
2021-05-26 23:31:45 +0500

    added gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,9 @@ let { src, dest } = require('gulp'),
     autoprefixer = require("gulp-autoprefixer"),
     group_media = require("gulp-group-css-media-queries"),
     clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename")
-    uglify = require("gulp-uglify-es").default
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    imagemin = require("gulp-imagemin")
 
 function browserSync() {
     browsersync.init({
@@ -98,19 +99,36 @@ function js() {
     .pipe(browsersync.stream())
 }
 
+function images() {
+    return src(path.src.img)
+    .pipe(
+        imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false}],
+            interlaced: true,
+            optimizationLevel: 3
+        })  
+    )
+    .pipe(fileinclude())
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
+
 function watchFiles(params) {
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
     gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.img], images)
 }
 
 function clean(params) {
     return del(path.clean)
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html))
+let build = gulp.series(clean, gulp.parallel(js, css, html, images))
 let watch = gulp.parallel(build, watchFiles, browserSync)
 
+exports.images
 exports.js = js
 exports.css = css
 exports.html = html
